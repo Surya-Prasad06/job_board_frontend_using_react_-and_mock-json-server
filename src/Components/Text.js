e305f025adc783d1c3494223c6daa14e9069833e
Surya-Prasad06 <suryaprasadsadige007@gmail.com>
2025-09-17 21:04:29 +0530

    first commit

--- src/Components/Text.js
+++ src/Components/Text.js
@@ -5,7 +5,7 @@ const Text = () => {
         <div class="container mt-5">
   <div class="marquee-container">
     <div class="marquee-text">
-      ⚠️ Don't use your real details. Use something like: Name: John Doe, Email: john@example.com, Password: 123456
+      ⚠️ Don't use your real details. Use something like: Name: ****, Email: ****@example.com, Password: ****
     </div>
   </div>
 </div>
